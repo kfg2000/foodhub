17d6a73037b7b53886807b1f18fdc0233625d76a
Khalid Al-Ghunaim <kfg2000@live.com>
2017-11-09 20:59:29 +0300

    task 3 done finally

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^list/$', views.restaurant_list, name = 'list'),
-    url(r'^detail/(?P<restaurant_id>\d+)/$', views.restaurant_detail, name = 'detail'),
+    url(r'^detail/(?P<restaurant_id>\d+)/$', views.restaurant_detail, name = 'restaurant_detail'),
+    url(r'^create/$', views.restaurant_create, name = 'restaurant_create'),
+    url(r'^update/(?P<restaurant_id>\d+)/$', views.restaurant_update, name = 'restaurant_update'),
+    url(r'^delete/(?P<restaurant_id>\d+)/$', views.restaurant_delete, name = 'restaurant_delete'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,16 +1,45 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Restaurant 
+from .forms import RestaurantForm
 # Create your views here.
 def restaurant_list(request):
-	kao = Restaurant.objects.all()
+	item = Restaurant.objects.all()
 	context = {
-	'stuff': kao
+	'items': item
 	}
 	return render(request, 'restaurant_list.html', context)
 
 def restaurant_detail(request,restaurant_id):
-	detail = get_object_or_404(Restaurant, id = restaurant_id)
+	item = get_object_or_404(Restaurant, id = restaurant_id)
 	context = {
-	'kao': detail
+	'item': item,
 	}
 	return render(request, 'restaurant_detail.html',context)
+
+
+def restaurant_create(request):
+	form = RestaurantForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect("list")
+	context = {
+	'form': form,
+	}
+	return render(request,"restaurant_create.html",context)
+
+def restaurant_update(request,restaurant_id):
+	item = Restaurant.objects.get(id=restaurant_id)
+	form = RestaurantForm(request.POST or None, instance=item)
+	if form.is_valid():
+		form.save()
+		return redirect("restaurant_detail", restaurant_id=item.id)
+	context = {
+	'form': form,
+	'item': item,
+	}
+	return render(request,"restaurant_update.html",context)
+
+def restaurant_delete(request,restaurant_id):
+	item = Restaurant.objects.get(id=restaurant_id).delete()
+	return redirect("list")
+	
